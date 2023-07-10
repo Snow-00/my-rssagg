d3232a74df446c9eb92d78ff3c210b532aa91ca9
Snow-00 <wariorwilly43@gmail.com>
2023-07-10 23:17:40 +0000

    starting server on port 8000

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-  "fmt"
   "os"
   "log"
+  "net/http"
   "github.com/joho/godotenv"
+  "github.com/go-chi/chi"
 )
 
 func main() {
-  fmt.Println("hello world")
-
   // by default it extract from file .env but we can specified the name file as well -> godotenv.Load(".env")
   godotenv.Load()  
 
@@ -18,5 +17,16 @@ func main() {
     log.Fatal("PORT is not found in env")
   }
 
-  fmt.Println("Port:", portString)
+  router := chi.NewRouter()
+
+  srv := &http.Server{
+    Handler: router,
+    Addr: ":" + portString,
+  }
+  
+  log.Printf("server starting on port %v", portString)
+  err := srv.ListenAndServe()
+  if err != nil{
+    log.Fatal(err)
+  }
 }
