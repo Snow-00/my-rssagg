071adf13d383cc19ef7ffa1516be561923831998
Snow-00 <wariorwilly43@gmail.com>
2023-07-14 18:16:38 +0700

    finish boilerplate (router)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "net/http"
   "github.com/joho/godotenv"
   "github.com/go-chi/chi"
+  "github.com/go-chi/cors"
 )
 
 func main() {
@@ -18,6 +19,24 @@ func main() {
   }
 
   router := chi.NewRouter()
+
+  router.Use(cors.Handler(cors.Options{
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"*"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300,
+  }))
+
+  v1Router := chi.NewRouter()
+  // this will make even POST request still get us a response (we want only GET request that gets response)
+  //v1Router.HandleFunc("/healthz", handlerReadiness)
+
+  v1Router.Get("/healthz", handlerReadiness)
+  v1Router.Get("/err", handlerErr)
+
+  router.Mount("/v1", v1Router)
 
   srv := &http.Server{
     Handler: router,
